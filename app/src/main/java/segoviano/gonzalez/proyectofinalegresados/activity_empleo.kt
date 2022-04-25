e40907dfd43a0325147c1979b3e74cead63339cd
Alejandro Gonzalez <luis.alejandro.gonzalez.bau@gmail.com>
2022-04-25 14:31:55 -0700

    activity empleo hecha

---
+++ app/src/main/java/segoviano/gonzalez/proyectofinalegresados/activity_empleo.kt
@@ -0,0 +1,13 @@
+package segoviano.gonzalez.proyectofinalegresados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import segoviano.gonzalez.proyectofinalegresados.ui.Empleo
+
+class activity_empleo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empleo)
+    }
+
+}
